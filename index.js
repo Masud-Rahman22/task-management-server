d2395fc731478fde70bf77c7f2e2cb9169abe6e2
Masud_Rahman22 <masudrhmn6560@gmail.com>
2023-12-22 03:46:45 +0600

    working on the update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -37,6 +37,36 @@ async function run() {
         app.get('/tasks', async (req, res) => {
             const result = await taskCollection.find().toArray()
             res.send(result)
+        })
+        app.get('/tasks/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id:new ObjectId(id)}
+            const result = await taskCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.delete('/tasks/:id',async(req,res)=>{
+            const id = req.params.id
+            const query = {_id : new ObjectId(id)}
+            const result = await taskCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.put('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedTasks = req.body;
+            const cards = {
+                $set: {
+                    title: updatedTasks.title,
+                    description: updatedTasks.description,
+                    deadline: updatedTasks.deadline,
+                    priority: updatedTasks.priority,
+                }
+            }
+            const result = await taskCollection.updateOne(filter, cards, options);
+            res.send(result);
         })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
